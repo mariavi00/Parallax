2bdf3fa676d5212d45190c358772a92195668b54
Parallax <mavi.gouveia678@gmail.com>
2024-11-08 11:11:10 -0300

    8/11

---
+++ Parallax/Animacao.cs
@@ -0,0 +1,67 @@
+namespace Parallax;
+
+public class Animacao
+{
+    protected List<String> animacao1 = new List<String>();
+    protected List<String> animacao2 = new List<String>();
+    protected List<String> animacao3 = new List<String>();
+    protected bool loop = true;
+    protected int animacaoAtiva = 1;
+    bool parado = true;
+    int frameAtual = 1;
+    protected Image compImage;
+    public Animacao(Image a)
+    {
+        compImage = a;
+    }
+    public void Stop()
+    {
+        parado = false;
+    }
+    public void Play()
+    {
+        parado = false;
+    }
+    public void SetanimacaoAtiva(int a)
+    {
+        animacaoAtiva = a;
+    }
+    public void Desenha()
+    {
+        if (parado)
+            return;
+        string nomeArquivo = "";
+        int tamanhoArquivo = 0;
+        if (animacaoAtiva == 1)
+        {
+            nomeArquivo = animacao1[frameAtual];
+            tamanhoArquivo = animacao1.Count;
+        }
+        else if (animacaoAtiva == 2)
+        {
+            nomeArquivo = animacao2[frameAtual];
+            tamanhoArquivo = animacao2.Count;
+        }
+        else if (animacaoAtiva == 3)
+        {
+            nomeArquivo = animacao3[frameAtual];
+            tamanhoArquivo = animacao3.Count;
+        }
+        compImage.Source = ImageSource.FromFile(nomeArquivo);
+        frameAtual++;
+        if (frameAtual >= tamanhoArquivo)
+        {
+            if (loop)
+                frameAtual = 0;
+            else
+            {
+                parado = true;
+                QuandoParar();
+            }
+        }
+    }
+    public virtual void QuandoParar()
+    {
+
+    }
+}
---
+++ Parallax/Player.cs
@@ -0,0 +1,9 @@
+namespace Parallax;
+public delegate void Callback();
+public class Player:Animacao
+{
+    public Player(Image a):base(a)
+    {
+        for (int i = 1; i <13;++i);
+    }
+}
